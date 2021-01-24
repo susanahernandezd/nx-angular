cfe5cbfbaee282f25f58b82ee758f9ef755cc062
Susana Hernandez <shernandez@plainconcepts.com>
2021-01-24 11:34:27 +0100

    Init NX Angular Todo app

---
+++ nx-todos/apps/todos/src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+interface Todo {
+  title: string;
+}
+
+@Component({
+  selector: 'nx-todos-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  todos: Todo[] = [];
+
+  constructor(private http: HttpClient) {
+    this.fetch();
+  }
+
+  fetch() {
+    this.http.get<Todo[]>('/api/todos').subscribe(t => this.todos = t);
+  }
+
+  addTodo() {
+    this.http.post('/api/addTodo', {}).subscribe(() => {
+      this.fetch();
+    });
+  }
+}
